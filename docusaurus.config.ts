e95f906257a128fe44101fff81d350b957eb1333
Amin Roslan <amnrsln@gmail.com>
2025-01-30 00:07:43 +0800

    feat: favicon and og

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
 
 	themeConfig: {
 		// Replace with your project's social card
-		image: 'img/docusaurus-social-card.jpg',
+		image: 'img/reservekit-og.png',
 		navbar: {
 			title: 'ReserveKit Docs',
 			logo: {
